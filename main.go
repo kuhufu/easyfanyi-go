5ce324503f4d635d3324473e1104a75f78bb48a9
kuhufu <huboemail@yeah.net>
2020-04-08 18:17:25 +0800

    update

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
 	"os"
+	"time"
 )
 
 func main() {
@@ -15,22 +18,6 @@ func main() {
 		os.Exit(0)
 	}
 
-	domain := "http://fanyi.youdao.com"
-
-	keyfrom := "easyfanyi"
-	key := "1929637537"
-	q := url.QueryEscape(os.Args[1])
-	myurl := domain + "/openapi.do?keyfrom=" + keyfrom + "&key=" + key + "&type=data&doctype=json&version=1.1&q=" + q
-
-	resp, err := http.Get(myurl)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer resp.Body.Close()
-
-	data, err := ioutil.ReadAll(resp.Body)
-
 	m := struct {
 		Translation []string `json:"translation"`
 		Basic       struct {
@@ -38,9 +25,12 @@ func main() {
 			Explains   []string `json:"explains"`
 		} `json:"basic"`
 	}{}
-	err = json.Unmarshal(data, &m)
+
+	ctx, _ := context.WithTimeout(context.Background(), time.Second)
+	err := Call(ctx, &m)
 	if err != nil {
-		panic("json unmarshal error")
+		fmt.Println(err)
+		return
 	}
 
 	if len(os.Args) == 2 {
@@ -56,4 +46,43 @@ func main() {
 			fmt.Println(v)
 		}
 	}
+}
+
+func Call(ctx context.Context, v interface{}) error {
+	done := make(chan error)
+
+	go func() {
+		err := call(v)
+		done <- err
+	}()
+
+	select {
+	case <-ctx.Done():
+		return errors.New("request time out")
+	case err := <-done:
+		return err
+	}
+}
+
+func call(v interface{}) error {
+	domain := "http://fanyi.youdao.com"
+
+	keyfrom := "easyfanyi"
+	key := "1929637537"
+	q := url.QueryEscape(os.Args[1])
+	myurl := domain + "/openapi.do?keyfrom=" + keyfrom + "&key=" + key + "&type=data&doctype=json&version=1.1&q=" + q
+
+	resp, err := http.Get(myurl)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	data, err := ioutil.ReadAll(resp.Body)
+
+	err = json.Unmarshal(data, v)
+	if err != nil {
+		return err
+	}
+	return nil
 }
